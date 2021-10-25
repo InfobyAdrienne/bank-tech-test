81e73147f313f522fcc6a1c98e81f1c41a476cd9
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-10-25 16:40:03 +0100

    Making the statement class return the timestamp

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,9 +1,12 @@
 require 'statement'
 require 'account'
 
-describe Statement do 
-  subject (:statement) { described_class.new }
+describe Statement do
+  let(:statement) { Statement.new(:amount, :account) }
   let(:timestamp) { Time.now.strftime('%d/%m/%Y') }
 
-  
+  it 'stores the date' do
+    expect(statement.timestamp).to eq timestamp
+  end
+
 end
